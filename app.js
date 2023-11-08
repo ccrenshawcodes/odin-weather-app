a5fb957b4c2445104e40d5f5add3809cfe0cdfea
Catherine Crenshaw <55335367+ccrenshawcodes@users.noreply.github.com>
2023-11-08 10:46:03 -0700

    Change page background color based on current weather

--- app.js
+++ app.js
@@ -33,6 +33,7 @@ goButton.addEventListener('click', async () => {
     nowLayout();
     let data = await getWeather(myLocation.q);
     setLocationTitle(data);
+    changePageAppearance(data);
     populateCurrentWeather(data);
     showAlerts(data);
 });
@@ -60,9 +61,10 @@ function showAlerts (weatherObj) {
 }
 
 function populateCurrentWeather (weatherObj) {
+    const foo = `temp_${myLocation.scale}`;
     let nowTemp = document.querySelector('.left');
     let nowCondition = document.querySelector('.right');
-    nowTemp.textContent = weatherObj.current.temp_f;
+    nowTemp.textContent = weatherObj.current[foo];
     nowCondition.textContent = weatherObj.current.condition.text;
 }
 
@@ -209,3 +211,74 @@ threeDaysButton.addEventListener('click', async () => {
     let data = await getWeather(myLocation.q);
     populateThreeDaysWeather(data);
 })
+
+function changePageAppearance (weatherObj) {
+    const background = document.querySelector('.content');
+    if (weatherObj.current.is_day === 0) {
+        background.style.cssText = 'background-color: var(--night);';
+    } else {
+        switch (weatherObj.current.condition.code) {
+            case 1000:
+            case 1003:
+                background.style.cssText = 'background-color: var(--sunny-color);';
+                break;
+            case 1006:
+            case 1009:
+            case 1030:
+                background.style.cssText = 'background-color: var(--cloudy-one);';
+                break;
+            case 1063:
+            case 1135:
+            case 1147:
+            case 1150:
+            case 1153:
+            case 1180:
+            case 1183:
+            case 1186:
+            case 1189:
+            case 1240:
+                background.style.cssText = 'background-color: var(--cloudy-two);';
+                break;
+            case 1168:
+            case 1171:
+            case 1192:
+            case 1195:
+            case 1201:
+            case 1243:
+            case 1246:
+                background.style.cssText = 'background-color: var(--precip-two);';
+                break;
+            case 1066:
+            case 1069:
+            case 1072:
+            case 1114:
+            case 1117:
+            case 1204:
+            case 1207:
+            case 1210:
+            case 1213:
+            case 1216:
+            case 1219:
+            case 1222:
+            case 1225:
+            case 1237:
+            case 1249:
+            case 1252:
+            case 1255:
+            case 1258:
+            case 1261:
+            case 1264:
+                background.style.cssText = 'background-color: var(--snowy);';
+                break;
+            case 1287:
+            case 1273:
+            case 1276:
+            case 1279:
+            case 1282:
+                background.style.cssText = 'background-color: var(--thunder);';
+                break;
+            default:
+                background.style.cssText = 'background-color: var(--cloudy-one);';
+        }
+    }
+}
